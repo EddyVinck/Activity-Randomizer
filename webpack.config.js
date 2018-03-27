f221cd558ff4f00242f3677cbbaa2200b490de68
Eddy Vinck <eddyvinck95@gmail.com>
2018-03-27 09:32:14 +0200

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+const webpack = require('webpack');
+const path = require('path');
+
+/*
+ * We've enabled UglifyJSPlugin for you! This minifies your app
+ * in order to load faster and run less javascript.
+ *
+ * https://github.com/webpack-contrib/uglifyjs-webpack-plugin
+ *
+ */
+
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+/*
+ * I've added HtmlWebpackPlugin
+ */
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index',
+
+	devServer: {
+		contentBase: "./dev",
+		compress: true,
+		port: "9000"
+	},
+	watchOptions: {
+		aggregateTimeout: 300,
+		poll: 1000,
+		ignored: /node_modules/ // this may fuck things up
+	},
+	devtool: 'inline-source-map',
+
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dev')
+	},
+
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+
+				options: {
+					presets: ['env']
+				}
+			},
+			{
+				test: /\.(scss|css)$/,
+
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader'
+					},
+					{
+						loader: 'sass-loader'
+					}
+				]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [
+					'file-loader'
+				]
+			},
+		]
+	},
+
+	plugins: [
+		new UglifyJSPlugin(),
+		new HtmlWebpackPlugin({
+			title: "Output management",
+			template: 'src/assets/html/index.html'
+		}),
+	]
+};
