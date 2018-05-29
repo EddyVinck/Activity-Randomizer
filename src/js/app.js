eb8c61767a12170dc7556ca49eaa578de99aa5d9
Eddy Vinck <eddyvinck95@gmail.com>
2018-05-29 20:23:15 +0200

    Apparently Bootstrap has a collapsible already, so I used that

--- src/js/app.js
+++ src/js/app.js
@@ -5,14 +5,3 @@ import 'bootstrap/js/src/modal';
 import 'bootstrap/js/src/collapse'; 
 import './cookies/cookies';
 // import dataToggleHeights from './frameworks/custom-bootstrap/data-toggle';
-const getDataToggle = () => import('./frameworks/custom-bootstrap/data-toggle');
-
-// dataToggleHeights();
-
-if(document.querySelectorAll('[data-toggle=height-expand]').length > 0) {
-  getDataToggle().then(f => {
-    f.default();
-  });
-} else {
-  console.log('no data toggles for height-expand');
-}
