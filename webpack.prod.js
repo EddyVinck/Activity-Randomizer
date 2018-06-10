3661d770953513b7281682e4675baeef50543fe8
Eddy Vinck <eddyvinck95@gmail.com>
2018-06-10 18:47:51 +0200

    Fixed Webpack chunks and updated production build process

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 // const devMode = process.env.NODE_ENV !== 'production';
@@ -10,13 +10,82 @@ module.exports = (env, argv) => {
 	console.log(`mode: ${argv.mode}`);
 
 	return {
-		entry: './src/index',
+		entry: {
+			'activityrandomizer': './src/js/activity-randomizer.js',
+			common: './src/js/common.js'
+		},
 
 		output: {
-			filename: '[name].bundle.js',
+			filename: '[name].entry.js',
 			path: path.resolve(__dirname, 'dist')
 		},
 
+		module: {
+			rules: [
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					include: path.resolve(__dirname, "src"),
+					loader: 'babel-loader',
+
+					options: {
+						presets: ['env'],
+						"plugins": ["syntax-dynamic-import"]
+					}
+				},
+				{
+					test: /\.(png|svg|jpg|gif)$/,
+					include: [
+						path.resolve(__dirname, 'src/assets/img')
+					],
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								name: '[name].[ext]',
+								outputPath: 'assets/images'
+							}
+						}
+					]
+				},
+				{
+					test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+					include: [
+						path.resolve(__dirname, 'src/assets/fonts')
+					],
+					use: [
+						{
+							loader: 'file-loader',
+							options: {
+								name: '[name].[ext]',
+								outputPath: 'assets/fonts/'
+							}
+						}
+					]
+				},
+				{
+					test: /\.(scss|css)$/,
+					use: [
+						
+						{ loader: MiniCssExtractPlugin.loader,
+							options: { publicPath: '../' } 
+						},
+						{
+							loader: 'css-loader'
+						},
+						{
+							loader: 'sass-loader'
+						}
+					]
+				}
+			]
+		},
+		resolve: {
+			alias: {
+				fontello: path.resolve(__dirname, 'src/assets/fonts/icons/fontello-icons'),
+				img: path.resolve(__dirname, 'src/assets/img')				
+			}
+		},
 		plugins: [
 			new webpack.ProvidePlugin({
 				$: 'jquery'
@@ -38,77 +107,13 @@ module.exports = (env, argv) => {
 				hash: true,
 				inject: false
 			}),
+			new HtmlWebpackPlugin({
+				filename: 'google-sheet-tutorial.html',				
+				template: 'src/assets/html/google-sheet-tutorial.html',
+				hash: true,
+				inject: false
+			}),
 		],
-
-		module: {
-			rules: [
-				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					include: path.resolve(__dirname, "src"),
-					loader: 'babel-loader',
-
-					options: {
-						presets: ['env']
-					}
-				},
-				{
-					test: /\.(png|svg|jpg|gif)$/,
-					include: [
-						path.resolve(__dirname, 'src/assets/img')
-					],
-					use: [
-						'file-loader'
-					]
-				},
-				{
-					test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
-					include: [
-						path.resolve(__dirname, 'src/assets/icons'),
-						path.resolve(__dirname, 'src/assets/fonts')
-					],
-					use: [
-						{
-							loader: 'file-loader',
-							options: {
-								name: '[name].[ext]',
-								outputPath: 'assets/fonts/'
-							}
-						}
-					]
-				},
-				{
-					test: /\.(scss|css)$/,
-					use: [
-						{ loader: MiniCssExtractPlugin.loader,
-							options: { publicPath: '../' } 
-						},
-						{
-							loader: 'css-loader'
-						},
-						{
-							loader: 'sass-loader'
-						}
-					]
-				}
-			]
-		},
-
-		devServer: {
-			contentBase: "./dev",
-			compress: true,
-			port: "9000"
-		},
-		watchOptions: {
-			aggregateTimeout: 300,
-			poll: 1000,
-			ignored: /node_modules/
-		},
-		resolve: {
-			alias: {
-				fontello: path.resolve(__dirname, 'src/assets/icons/fontello-icons')
-			}
-		}
 		// devtool: 'source-map' // CSS source not shown in devtools
 	}
 };
