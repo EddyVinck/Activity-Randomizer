1e732e4bfaddf953e1f5032f316876e2ab9b766c
Eddy Vinck <eddyvinck95@gmail.com>
2018-07-10 20:22:16 +0200

    disabled no-param-reassign as advised https://github.com/airbnb/javascript/issues/1861

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,19 @@
 module.exports = {
-  extends: "airbnb-base",
-  rules:{
-    "linebreak-style": 0
+  "extends": "airbnb-base",
+  "rules":{
+    "linebreak-style": 0,
+    "no-param-reassign": 0,
+    "no-use-before-define": ["warn", { 
+      "functions": false,
+      "classes": true,
+      "variables": true }],
   },
   "env": {
     "browser": true,
     "node": true,
     "jasmine": true
   },
-  globals: {
+  "globals": {
     "gapi": true
-  }
+  },  
 };
